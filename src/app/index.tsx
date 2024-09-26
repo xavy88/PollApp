1aaf2f11d4cc3b149435808b01ef5d5040b233a6
Xavy <xavyurbina88@gmail.com>
2024-09-26 15:46:06 -0600

    Initial Poll list in home screen implemented

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Stack } from 'expo-router';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const polls = [1, 2, 3];
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Xavy!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Stack.Screen options={{title:'Polls',
+      headerStyle:{
+        backgroundColor:'#09b4e8',
+      },
+      headerTintColor:'#fff',
+      headerTitleStyle:{
+        fontWeight:'bold',
+        fontSize:25,
+      },
+    }} />
+     <FlatList
+        data={polls}
+        contentContainerStyle={styles.container}
+        renderItem={() => (
+          <View style={styles.pollContainer} >
+            <Text style={styles.pollTitle} >Question Poll Example</Text>
+          </View>
+        )}
+      />
+      </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0b0652',
+    padding: 10,
+    gap:5,
+  },
+  pollContainer: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#09b4e8',
+  },
+  pollTitle: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
